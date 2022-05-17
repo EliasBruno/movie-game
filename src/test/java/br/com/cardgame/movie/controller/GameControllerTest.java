975ca7a3e16ebce7c647d5d4be1a5cb3c7830e33
Elias Bruno C. Carneiro <elias.carneiro@grupoboticario.com.br>
2022-05-17 13:47:33 -0300

    Added more tests in select method

--- src/test/java/br/com/cardgame/movie/controller/GameControllerTest.java
+++ src/test/java/br/com/cardgame/movie/controller/GameControllerTest.java
@@ -1,5 +1,6 @@
 package br.com.cardgame.movie.controller;
 
+import br.com.cardgame.movie.component.MovieComponent;
 import br.com.cardgame.movie.controller.request.GameMoviePatch;
 import br.com.cardgame.movie.controller.request.LoginRequest;
 import br.com.cardgame.movie.controller.response.TokenResponse;
@@ -12,15 +13,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.ArrayList;
 import java.util.Date;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -40,7 +45,8 @@ public class GameControllerTest {
 
     @Autowired
     private ObjectMapper objectMapper;
-
+    @MockBean
+    MovieComponent movieComponent;
     private static String url = "/game-movie";
     private TokenResponse tokenResponse;
 
@@ -63,6 +69,7 @@ public class GameControllerTest {
 
     @Test
     public void give_select_game_success() throws Exception {
+        when(movieComponent.getMovieOkay(Mockito.any())).thenReturn("Batman");
         TokenResponse tokenResponse = getToken();
         save_game();
         GameMoviePatch gameMoviePatch = new GameMoviePatch();
@@ -75,6 +82,25 @@ public class GameControllerTest {
                 .andReturn();
         Game game = objectMapper.readValue(result.getResponse().getContentAsString(), Game.class);
         Assertions.assertEquals("Batman", game.getSelected());
+        Assertions.assertEquals(1, game.getSuccess());
+    }
+
+    @Test
+    public void give_select_game_errors() throws Exception {
+        when(movieComponent.getMovieOkay(Mockito.any())).thenReturn("Hulk");
+        TokenResponse tokenResponse = getToken();
+        save_game();
+        GameMoviePatch gameMoviePatch = new GameMoviePatch();
+        gameMoviePatch.movie = "Batman";
+        MvcResult result= mockMvc.perform(patch(url+"/1/select")
+                .header("Authorization", "Bearer "+ tokenResponse.getToken())
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(gameMoviePatch)))
+                .andExpect(status().isOk())
+                .andReturn();
+        Game game = objectMapper.readValue(result.getResponse().getContentAsString(), Game.class);
+        Assertions.assertEquals("Batman", game.getSelected());
+        Assertions.assertEquals(1, game.getErrors());
     }
 
     @Test
